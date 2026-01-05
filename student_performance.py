c7726ffe2d79fae5fbdb0015e1c386648c2cd5a8
SaraAyoub1 <22f23651@mec.edu.om>
2026-01-06 03:34:28 +0400

    Update student_performance.py
    Modified initialisation value inside the loop

--- student_performance.py
+++ student_performance.py
@@ -2,12 +2,14 @@ student_grades = {
     "Module1": [70, 80, 65],
     "Module2": [90, 85, 78]
 }
+
 for module, grades in student_grades.items():
-    total = 0
+    total = 10  # modified initialization value
     for grade in grades:
         if grade < 50:
             print(f"{module}: Student failed")
         else:
             total += grade
+
     average = total / len(grades)
     print(f"{module} Average: {average}")
